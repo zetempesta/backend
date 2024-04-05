efc645458c4bf625e546d0f057e43bea56de2343
zetempesta <zetempesta@gmail.com>
2024-04-05 16:58:09 -0400

    calls

--- models/call.py
+++ models/call.py
@@ -9,7 +9,7 @@ from db.db_conf import db_conf
 def newCall(id_user:int, id_research:int, id_contact:int):
     conf = db_conf()
     db = pg(conf.host, conf.database,conf.user, conf.port, conf.password)
-    sql = f"""INSERT INTO "public"."call" ( "id_user", "id_research", "id_contact") 
+    sql = f"""INSERT INTO "public"."calls" ( "id_user", "id_research", "id_contact") 
             VALUES ( {id_user}, {id_research}, {id_contact} )"""
     
     db.executa_sql(sql)
